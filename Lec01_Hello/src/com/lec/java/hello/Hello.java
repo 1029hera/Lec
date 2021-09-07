427df3333a5741fad8aeee6aa483aaa0f692382d
JiweonYoo <81316013+1029hera@users.noreply.github.com>
2021-09-07 18:14:25 -0500

    Add files via upload

---
+++ Lec01_Hello/src/com/lec/java/hello/Hello.java
@@ -0,0 +1,25 @@
+package com.lec.java.hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// 이것은 주석(comment) 입니다.
+		System.out.println("Hello Java!");
+		
+		System.out.println("안녕하세요 ");
+		System.out.println("자바");
+		
+		System.out.println(); // 한 라인만 줄바꿈 함
+		System.out.println();
+		
+		System.out.println("히니꺼");
+		
+		System.out.println(1 + 2);
+		System.out.println(1024);
+		
+		System.out.println("안녕하세요" + " 1일이예요."); // "문자열" + "문자열" => 문자열 연결된 결과
+		System.out.println("1" + "2");
+		
+	} // end main()
+
+} // end class
