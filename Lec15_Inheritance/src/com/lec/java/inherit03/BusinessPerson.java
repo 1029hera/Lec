01bce5343797cf48fcd675ac3812276ef0cac9cb
JiweonYoo <81316013+1029hera@users.noreply.github.com>
2021-09-07 18:19:47 -0500

    Add files via upload

---
+++ Lec15_Inheritance/src/com/lec/java/inherit03/BusinessPerson.java
@@ -0,0 +1,15 @@
+package com.lec.java.inherit03;
+
+//Object
+//	└ Person
+//    	 └  BusinessPerson
+
+public class BusinessPerson extends Person {
+	
+	String company;
+	
+	public void shoInfo() {
+		whoAmI();
+		System.out.println("회사는 " + company + " 입니다.");
+	}
+}
---
+++ Lec15_Inheritance/src/com/lec/java/inherit07/PostBoy.java
@@ -0,0 +1,6 @@
+package com.lec.java.inherit07;
+
+// final class는 더이상 상속 불과
+public class PostBoy { //extends PostPerson {
+
+}
